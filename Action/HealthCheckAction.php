b055085ea040a481b2d00814488722f2a9a57035
Jonathan VUILLEMIN <ekkinox@gmail.com>
2020-01-28 19:49:10 +0100

    Basic checkers components implementation + bundle initialisation

---
+++ Action/HealthCheckAction.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace OAT\Bundle\HealthCheckBundle\Action;
+
+use OAT\Library\HealthCheck\HealthChecker;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HealthCheckAction
+{
+    /** @var HealthChecker */
+    private $checker;
+
+    public function __construct(HealthChecker $checker)
+    {
+        $this->checker = $checker;
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $results = $this->checker->performChecks();
+
+        return new JsonResponse(
+            $results,
+            $results->hasErrors() ? Response::HTTP_INTERNAL_SERVER_ERROR : Response::HTTP_OK
+        );
+    }
+}
